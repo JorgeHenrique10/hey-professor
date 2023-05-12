2b229270c826975dfa3c0bb1336d4465c03d1480
Jorge Henrique <jorgehenriquejds@gmail.com>
2023-05-11 22:43:31 -0300

    it shold have at least 10 characters

--- tests/Feature/CreateAQuestionTest.php
+++ tests/Feature/CreateAQuestionTest.php
@@ -22,4 +22,14 @@ it('shold check if ends with question mark ?', function () {
 });
 
 it('shold have at least 10 characters', function () {
+    $user = User::factory()->create();
+
+    actingAs($user);
+
+    $request = post(route('question.store', [
+        'question' => str_repeat('*', 8) . '?',
+    ]));
+
+    $request->assertSessionHasErrors(['question' => __('validation.min.string', ['min' => 10, 'attribute' => 'question'])]);
+    assertDatabaseCount('questions', 0);
 });

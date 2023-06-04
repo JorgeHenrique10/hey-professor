614371a075ecb950fa5da85aae8fdbc1ac6452c7
Jorge Henrique <jorgehenriquejds@gmail.com>
2023-06-04 15:00:07 -0300

    should make sure that only the person who has created the question can edit the question

--- tests/Feature/Question/EditTest.php
+++ tests/Feature/Question/EditTest.php
@@ -7,7 +7,7 @@ use function Pest\Laravel\{actingAs, get};
 it('should be able to open a question to edit', function () {
     $user = User::factory()->create();
 
-    $question = Question::factory()->for($user, 'createdBy')->create();
+    $question = Question::factory()->for($user, 'createdBy')->create(['draft' => true]);
 
     actingAs($user);
 
@@ -19,11 +19,35 @@ it('should be able to open a question to edit', function () {
 it('should return a view', function () {
     $user = User::factory()->create();
 
-    $question = Question::factory()->for($user, 'createdBy')->create();
+    $question = Question::factory()->for($user, 'createdBy')->create(['draft' => true]);
 
     actingAs($user);
 
     $request = get(route('question.edit', $question));
 
     $request->assertViewIs('question.edit');
+});
+
+it('should make sure that only wuestion with status DRAFT can be edited', function () {
+    $user = User::factory()->create();
+
+    $questionNotDraft = Question::factory()->for($user, 'createdBy')->create(['draft' => false]);
+    $questionDraft    = Question::factory()->for($user, 'createdBy')->create(['draft' => true]);
+
+    actingAs($user);
+
+    get(route('question.edit', $questionNotDraft))->assertForbidden();
+    get(route('question.edit', $questionDraft))->assertSuccessful();
+});
+
+it('should make sure that only the person who has created the question can edit the question', function () {
+    $rightUser = User::factory()->create();
+    $wrongUser = User::factory()->create();
+    $question  = Question::factory()->create(['draft' => true, 'created_by' => $rightUser->id]);
+
+    actingAs($wrongUser);
+    get(route('question.edit', $question))->assertForbidden();
+
+    actingAs($rightUser);
+    get(route('question.edit', $question))->assertSuccessful();
 });
